357c25ebbe1cd1e448ec32c57a9c19096519feb9
nin0dev <support@nin0dev.com>
2024-02-27 09:29:00 -0500

    renamed ping function

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
+import sqlite3
 
 app = FastAPI()
+connection = sqlite3.connect("database.db")
 
 
 @app.get("/")
-async def root():
+async def ping():
     return {"success": True}
+
+
