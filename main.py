db781bd89da18b48b73de229e3ce13ebe9bbce9a
nin0dev <support@nin0dev.com>
2024-02-27 18:59:04 -0500

    added ability to add scans

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ async def ping():
     return {"success": True}
 
 
+# <editor-fold desc="List management">
 @app.get("/list/create")
 async def create_list():
     # Get list code
@@ -26,7 +27,7 @@ async def create_list():
 
 
 @app.get("/list/check")
-async def check_list(list_code: str = "invalid"):
+async def check_list(list_code: str = None):
     # Query specific list code
     cursor.execute(f"SELECT list_code FROM listcodes WHERE list_code='{list_code}'")
     result = cursor.fetchone()
@@ -37,3 +38,33 @@ async def check_list(list_code: str = "invalid"):
     else:
         # The list doesn't exist
         return {"success": False}
+# </editor-fold>
+
+
+@app.get("/scan/add")
+async def add_scan(list_code: str = None, scan_value: str = None):
+    if list_code is None or scan_value is None:
+        return {"success": False}
+    # Check if list is existing
+    cursor.execute(f"SELECT list_code FROM listcodes WHERE list_code='{list_code}'")
+    result = cursor.fetchone()
+    if result is None:
+        return {"success": False}
+    # Create scans table (if needed)
+    cursor.execute("CREATE TABLE IF NOT EXISTS 'scans' (list_code CHAR(8), value VARCHAR(255), scan_count INTEGER);")
+    # Set scan count
+    scan_count = 1
+    cursor.execute(f"SELECT scan_count FROM scans WHERE value='{scan_value}' AND list_code='{list_code}'")
+    result = cursor.fetchone()
+    if result is not None:
+        # Entry already exists
+        scan_count = result[0] + 1
+        cursor.execute(f"UPDATE scans SET scan_count = {scan_count} WHERE value='{scan_value}' AND list_code='{list_code}'")
+        connection.commit()
+        return {"success": True, "scan_value": scan_value, "scan_count": scan_count}
+    else:
+        # Entry doesn't exist, so we need to create it
+        cursor.execute(f"INSERT INTO scans VALUES ('{list_code}', {scan_value}, {scan_count});")
+        connection.commit()
+        return {"success": True, "scan_value": scan_value, "scan_count": scan_count}
+
